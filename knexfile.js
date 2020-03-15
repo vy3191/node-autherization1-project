cb99cec09a8590513dce746069a8889ad0272ea4
vy3191 <venkyafhs@gmail.com>
2020-03-14 21:14:44 -0400

    Added knex file

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    useNullAsDefault: true,
+    client: 'sqlite3',
+    connection: {
+      filename: './data/users.db3'
+    }
+  },  
+};
+
+
