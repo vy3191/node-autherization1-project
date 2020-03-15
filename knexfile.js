130084a9733f9aac46f4e11bba581a7d906bcb33
vy3191 <venkyafhs@gmail.com>
2020-03-14 21:23:12 -0400

    Added migrations file and modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,18 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/users.db3'
-    }
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      },
+    },
   },  
 };
 
