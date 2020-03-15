d46f872a897fa9e137b21f77e733e4c7859bf60e
vy3191 <venkyafhs@gmail.com>
2020-03-14 20:43:22 -0400

    Completed-welcome route

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const helmet = require('helmet');
 const logger = require('morgan');
 const server = express();
 
+const welcomeRouter = require('./welcome/welcome-router');
+
 server.use(helmet());
 server.use(logger('tiny'));
 server.use(express.json());
-server.get('/', (req,res,next) =>{msg:`App is up and running now`});
+server.use("/welcome", welcomeRouter);
+
 server.use((req,res,next) => res.status(404).json({msg:'Route Not Found'}));
 server.use((err,req,res,next) => res.status(500).json({msg:'Error 500-something wrong'}));
 
