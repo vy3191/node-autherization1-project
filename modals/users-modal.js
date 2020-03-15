281cdf9832be9d24a9b9d9f5fbd2fe341e804871
vy3191 <venkyafhs@gmail.com>
2020-03-14 22:11:37 -0400

    created  users-modal

---
+++ modals/users-modal.js
@@ -0,0 +1,28 @@
+const db = require('../data/config');
+
+async function find() {
+  const users = await db('users').select();
+  return users;
+}
+
+async function findById(id) {
+   const user = await db('users')
+                      .where({id})
+                      .select("id", "username", "password")
+                      .first();
+  return user;                      
+}
+
+async function findBy(filter) {
+   const users = await db('users')
+                        .select("id", "username", "password")
+                       .where(filter);
+   return users;                       
+};
+
+async function add(user) {
+   const [id] = await db('users').insert(user);
+   const newUser = await db('users').where({id}).select().first();
+   return newUser;
+}
+
